5a3de62b3a40bb29866ff769a0bb324c6ed36738
Soufiane Maakouk <soufianemaakouk@gmail.com>
2025-08-25 20:36:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CoursesSection from './CoursesSection';
 import ExercisesSection from './ExercisesSection';
 import './app.css';
 
 function App() {
+  const [selectedUnit, setSelectedUnit] = useState(1); // default unit 1
+
   return (
     <div className="container">
       <h1>Language Learning App</h1>
       <div className="sections">
         <CoursesSection />
-        <ExercisesSection />
+        <ExercisesSection
+          selectedUnit={selectedUnit}
+          onSelectUnit={setSelectedUnit}
+        />
       </div>
     </div>
   );
